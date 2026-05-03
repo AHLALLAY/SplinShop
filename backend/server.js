38aa9272d23e2081723118a84cc0002b80dfca5d
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-05-03 01:43:31 +0100

    feat: Appel de la connexion BDD dans server.js avec gestion propre de l'arrêt (Ctrl+C)

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,24 @@
 import express from 'express';
 import 'dotenv/config';
-
+import db from './databases/connection.js';
 
 // constants
-const PORT = 3000;
-const API_BASE_URL = '/api/v1';
+const PORT = process.env.PORT || 3000;
+const API_BASE_URL = process.env.API_BASE_URL || '/api/v1';
 const URI = process.env.DATABASE_URL;
 
 const app = express();
 
+app.use(express.json());
+
 const run = async () => {
     try {
         // connecting to database
-
+        await db.connectToDb();
 
         // creating defealt admin
 
-        
+
         // listening
         app.listen(PORT, () => {
             console.info(`server running on http://localhost:${PORT}/`);
@@ -27,3 +29,9 @@ const run = async () => {
 }
 
 run();
+
+// Gérer l'arrêt propre du serveur (Ctrl+C)
+process.on('SIGINT', async () => {
+    await db.disconnect();
+    process.exit(0)
+})
