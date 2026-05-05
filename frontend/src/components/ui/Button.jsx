e37537604def53f3fbde87fdbc4f85a4b68df7fb
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-05-05 15:19:32 +0100

    refact: Ajouter 'nullish operator' où cas où 'className' null

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
-export default function Button({className, type="button", onClick}){
+export default function Button({ children, className, type = "button", onClick }) {
     const default_style = "bg-blue-300 text-white font-bold";
     return (
-        <button className={`default_style ${className}`} type={type} onClick={onClick}>
+        <button className={`${default_style} ${className ?? ''}`} type={type} onClick={onClick}>
             {children}
         </button>
     );
