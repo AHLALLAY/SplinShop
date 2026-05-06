367575d6c133dff9857660e17562af343bea3a25
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-05-07 00:41:30 +0100

    feat: Ajouter les vues pour la page d'accueil et le tableau de bord admin

---
+++ frontend/src/views/index.jsx
@@ -0,0 +1,8 @@
+import CatalogCard from "../components/catalog/CatalogCard";
+
+export default function Vitrine(){
+    return (
+        // <CatalogCard data={}/>
+        <p>this home</p>
+    );
+}
