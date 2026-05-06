ff2cb68c1ea9acb77db6449af8ce0b0254d35507
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-05-06 16:50:49 +0100

    feat: Ajouter le champ 'name' au modèle User et recréer la migration correspondante

--- backend/prisma/migrations/20260506154231_user/migration.sql
+++ backend/prisma/migrations/20260506154231_user/migration.sql
@@ -4,6 +4,7 @@ CREATE TYPE "Role" AS ENUM ('customer', 'seller', 'admin');
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "role" "Role" NOT NULL DEFAULT 'customer',
