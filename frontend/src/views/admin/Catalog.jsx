ea54c462bd4b69f370e82eca3c9ce3da8021db0a
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-05-08 22:24:22 +0100

    feat: Ajouter un bouton pour afficher le CatalogModal dans le composant Catalog
    - Intégration d'un état local pour gérer la visibilité du modal
    - Ajout d'un bouton "Ajouter" pour ouvrir le CatalogModal

--- frontend/src/views/admin/Catalog.jsx
+++ frontend/src/views/admin/Catalog.jsx
@@ -1,12 +1,21 @@
+import Button from "../../components/ui/Button";
+import CatalogModal from "../../components/catalog/CatalogModal";
+import { useState } from "react";
 
 export default function Catalog() {
+    const [show, setshow] = useState(false);
+
+
     return (
         <div className="flex flex-col">
-            <div>
-                
+            <div className="flex justify-between">
+                <h1 className="text-amber-600 font-bold text-2xl">Catalogue</h1>
+                <Button className="px-2" onClick={() => setshow(true)}>
+                    {"Ajouter"}
+                </Button>
             </div>
             <div className="flex-1">
-
+                <CatalogModal visibility={show} onClose={() => setshow(false)} />
             </div>
         </div>
     );
