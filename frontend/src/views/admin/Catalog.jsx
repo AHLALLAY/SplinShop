d5a848de1676f1e44d4403197b5b8f8227b219f3
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-05-08 20:55:12 +0100

    feat: Ajouter les composants Catalog et Seller pour la gestion des vendeurs dans le module admin
    - Suppression du fichier .gitkeep dans le répertoire admin
    - Création du composant Catalog pour afficher le catalogue
    - Création du composant Seller avec un formulaire pour ajouter des vendeurs

---
+++ frontend/src/views/admin/Catalog.jsx
@@ -0,0 +1,13 @@
+
+export default function Catalog() {
+    return (
+        <div className="flex flex-col">
+            <div>
+                
+            </div>
+            <div className="flex-1">
+
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/views/admin/Seller.jsx
@@ -0,0 +1,23 @@
+import Button from "../../components/ui/Button";
+
+
+export default function Seller() {
+    const handleSubmition = async (e) => {
+
+    }
+    return (
+        <div className="flex flex-col">
+            <div>
+                <form onSubmit={handleSubmition} className="flex justify-between">
+                    <h1 className="text-amber-600 font-bold text-2xl">Vendeurs</h1>
+                    <Button type="submit" className="px-2">
+                        {"Ajouter"}
+                    </Button>
+                </form>
+            </div>
+            <div className="flex-1">
+                
+            </div>
+        </div>
+    );
+}
