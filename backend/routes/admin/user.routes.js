2cfd2e9b8c80423254668c45ca90f62bfa7bd540
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-05-13 22:12:38 +0100

    feat: Intégrer le midlleware d'authorisation dans user.routes

--- backend/routes/admin/user.routes.js
+++ backend/routes/admin/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import userController from "../../controllers/admin/user.controller.js";
 import isAuthenticated from "../../middlewares/auth.middleware.js";
+import requireAdmin from "../../middlewares/admin.middleware.js";
 
 const router = express.Router();
 
-router.post('/', isAuthenticated, userController.addSeller)
-router.get('/', isAuthenticated, userController.getSellers)
+router.post('/', isAuthenticated, requireAdmin, userController.addSeller);
+router.get('/', isAuthenticated, requireAdmin, userController.getSellers);
 
 export default router;
