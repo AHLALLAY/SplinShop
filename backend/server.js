bf9ce564d8ea6f049d71065cb89277cbc13023fd
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-05-13 17:53:21 +0100

    fix: Mettre à jour CORS origin pour utilise la variable d'enveronement.

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
