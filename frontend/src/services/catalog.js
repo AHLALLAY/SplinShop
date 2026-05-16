f82408cc22c66c442dbfd1d78b587eb6d9c258b4
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-05-16 22:31:59 +0100

    feat: Mise à jour de la méthode addCatalog pour utiliser FormData
    - Changement de la structure de données envoyées lors de l'ajout d'un catalogue pour utiliser FormData
    - Ajout de la gestion de l'image dans le corps de la requête si elle est fournie

--- frontend/src/services/catalog.js
+++ frontend/src/services/catalog.js
@@ -4,12 +4,12 @@ class Catalog {
     async loadCatalog() { return await useEndPoint('/catalog'); }
 
     async addCatalog(catalog) {
-        const body = {
-            name: catalog.name,
-            slug: catalog.slug,
-            pic: catalog.pic,
-            description: catalog.description
-        };
+        const body = new FormData();
+        body.append('name', catalog.name);
+        body.append('slug', catalog.slug);
+        body.append('description', catalog.description);
+        if(catalog.image) body.append('image', catalog.image);
+        
         const response = await useEndPoint('/catalog', "POST", body);
         return response.data;
     }
