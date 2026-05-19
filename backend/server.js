de16d717fdabded681cd1453fe88e710f06a5a66
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-05-19 23:17:42 +0100

    refactor: Simplifier le démarrage du serveur et améliorer la gestion des erreurs
    - Refactorisation de server.js pour importer app et la configuration depuis des modules séparés.
    - Amélioration de la gestion des erreurs avec des messages plus clairs.
    - Utilisation du port défini dans la nouvelle configuration pour l'écoute du serveur.

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+import app from './app.js';
+import { config } from './config/index.js';
 import db from './databases/connection.js';
-import createDefaultAdmin from './utils/addAdmin.js';
-import authRoute from './routes/auth/auth.routes.js';
-import userRoute from './routes/admin/user.routes.js';
-import catalogRoute from './routes/admin/catalog.routes.js';
-import productRoute from './routes/admin/product.routes.js';
-
-// constants
-const PORT = process.env.PORT || 3000;
-const API_BASE_URL = process.env.API_BASE_URL || '/api/v1';
-
-const app = express();
-
-app.use(express.json());
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-}));
-
-// routes
-app.use(`${API_BASE_URL}/auth`, authRoute);
-app.use(`${API_BASE_URL}/seller`, userRoute);
-app.use(`${API_BASE_URL}/catalog`, catalogRoute);
-app.use(`${API_BASE_URL}/product`, productRoute);
+import createDefaultAdmin from './utils/defaultAdmin.js';
 
 const run = async () => {
     try {
-        // connecting to database
         await db.connectToDb();
-
-        // creating defealt admin
         await createDefaultAdmin();
 
-        // listening
-        app.listen(PORT, () => {
-            console.info(`server running on http://localhost:${PORT}/`);
+        app.listen(config.PORT, () => {
+            console.info(`Server listening on http://localhost:${config.PORT}/`);
         });
     } catch (e) {
-        console.error(e);
+        console.error(`Server error: ${e.message}`);
         process.exit(1);
     }
 };
 
 run();
 
-// Gérer l'arrêt propre du serveur (Ctrl+C)
 process.on('SIGINT', async () => {
     await db.disconnect();
-    process.exit(0)
-})
+    process.exit(0);
+});
