11fe1e12fc412b79c986af4cd094405006e65f47
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-06-02 16:19:03 +0100

    refact: Améliorer le controlleur d'authentification - Renommer le fichier 'auth.controller.js' par 'auth.ctrl.js'. - Renommer la class 'AuthController' par AuthCtrl'. - Utiliser UpperCase au début des messages. - Ajouter JSDoc pour documenter les mèthodes.

--- backend/modules/auth/auth.ctrl.js
+++ backend/modules/auth/auth.ctrl.js
@@ -2,26 +2,39 @@ import authService from './auth.service.js';
 import { asyncHandler } from '../../utils/asyncHandler.js';
 import { sendApiResponse } from '../../utils/apiResponse.js';
 
-class AuthController {
+/**
+ * Contrôleur pour l'authentification.
+ */
+class AuthCtrl {
+    /**
+     * Connecte un utilisateur.
+     * @param {import('express').Request} req 
+     * @param {import('express').Response} res 
+     */
     login = asyncHandler(async (req, res) => {
         const user = await authService.login(req.body);
         sendApiResponse(res, {
             status: 200,
             success: true,
-            message: 'connecté',
+            message: 'Connecté',
             data: user,
         });
     });
 
+    /**
+     * Inscrit un nouveau client.
+     * @param {import('express').Request} req 
+     * @param {import('express').Response} res 
+     */
     register = asyncHandler(async (req, res) => {
-        const user = await authService.registerCustomer(req.body);
+        const user = await authService.register(req.body);
         sendApiResponse(res, {
             status: 201,
             success: true,
-            message: 'compte créé',
+            message: 'Compte créé',
             data: user,
         });
     });
 }
 
-export default new AuthController();
+export default new AuthCtrl();
